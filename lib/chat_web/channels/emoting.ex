987bc0a3f0ee8cadd13d649b9f259b4f4c004794
Chris White <chris.white@hover.to>
2019-12-13 15:32:33 -0700

    ♻️ Uses String.split correctly over String.graphemes for readability
    add elixir-ls to gitignore
    
    Co-authored-by: Matti Salokangas <matti.salokangas@codingzeal.com>

--- lib/chat_web/channels/emoting.ex
+++ lib/chat_web/channels/emoting.ex
@@ -1,12 +1,14 @@
 defmodule ChatWeb.Emoting do
   require Logger
 
-  @emojis String.graphemes(
-    "🐮🦆🐹🦊🐤🐝🐊🐆🐓🌴❄️🍋🌯🥪🍿🥜🍺🥤🥢⚽️🏀🏈🏅🎮🚗🚦"
-  )
+  @emojis String.split(
+            "🐮🦆🐹🦊🐤🐝🐊🐆🐓🌴❄️🍋🌯🥪🍿🥜🍺🥤🥢⚽️🏀🏈🏅🎮🚗🚦",
+            "",
+            trim: true
+          )
 
   def generate_user_id do
-    Enum.map(1..2, fn(_) -> [Enum.random(@emojis)] end)
+    Enum.map(1..2, fn _ -> [Enum.random(@emojis)] end)
     |> Enum.join("")
   end
 
@@ -14,15 +16,16 @@ defmodule ChatWeb.Emoting do
     "👋👋👋👋👋👋"
   end
 
-  def get_character(input = " ") do
+  def get_character(_input = " ") do
     "   "
   end
-  def get_character(input) do
+
+  def get_character(_input) do
     Enum.random(@emojis)
   end
 
   def emojify_message(message) do
-    String.graphemes(message)
+    String.split(message, "", trim: true)
     |> Enum.map(&get_character/1)
     |> Enum.join("")
   end
